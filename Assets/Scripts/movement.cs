2ccd3291da674dc9b6d6250fdd44898c34ca39c5
Dominik Würsch <d.wuersch@gmail.com>
2014-08-31 15:42:11 +0200

    Add TexturePacker Importer, movement for blendtree

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -9,9 +9,11 @@ public class movement : MonoBehaviour
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal") * Time.deltaTime;
 		float moveVertical = Input.GetAxis ("Vertical") * Time.deltaTime;
-		
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rigidbody2D.velocity = movement * speed;
-
+//		if (Mathf.Approximately(moveHorizontal, 0.0f) && Mathf.Approximately(moveVertical, 0.0f)) {
+//			rigidbody2D.velocity = new Vector2();
+//		} else {
+			Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+			rigidbody2D.velocity = movement * speed;
+//		}
 	}
 }
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,37 +3,83 @@ using System.Collections;
 
 public class playerController : MonoBehaviour
 {
+		public float dampTime90 = .2f;
+		private Animator animator;
+		public bool diagonal;
+		// Use this for initialization
+		void Start ()
+		{
+				animator = this.GetComponent<Animator> ();
+
+		}
 	
-	private Animator animator;
-	
-	// Use this for initialization
-	void Start()
-	{
-		animator = this.GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update()
-	{
+		// Update is called once per frame
+		void Update ()
+		{
+		Vector2 normalizedVelocity = transform.parent.rigidbody2D.velocity.normalized;
+
+		Vector2 controlVector = Mathf.Approximately (normalizedVelocity.sqrMagnitude, 0.0f) ? new Vector2 () : new Vector2 (normalizedVelocity.x+0.05f, normalizedVelocity.y + 0.05f);
+		Debug.Log (controlVector.ToString ());
+		animator.SetFloat ("velocityY", controlVector.y, dampTime90, Time.deltaTime);
+		animator.SetFloat ("velocityX", controlVector.x, dampTime90, Time.deltaTime);
 		
-		var vertical = Input.GetAxis("Vertical");
-		var horizontal = Input.GetAxis("Horizontal");
-		
-		if (vertical > 0)
-		{
-			animator.SetInteger("Direction", 2);
+//				var vertical = Input.GetAxis ("Vertical");
+//				var horizontal = Input.GetAxis ("Horizontal");
+//
+//				if ((horizontal < 0 && vertical < 0) | (horizontal > 0 && vertical < 0) | (horizontal > 0 && vertical > 0) | (horizontal < 0 && vertical > 0)) {
+//						diagonal = true;
+//				} else {
+//						diagonal = false;
+//				}
+//
+//				if (diagonal == false) {
+//
+//						if (vertical > 0) {
+//								animator.SetFloat ("velocityY", 1, dampTime90, Time.deltaTime);
+//								animator.SetFloat ("velocityX", 0, dampTime90, Time.deltaTime);
+//								print ("north");
+//						} else if (vertical < 0) {
+//								animator.SetFloat ("velocityY", -1, dampTime90, Time.deltaTime);
+//								animator.SetFloat ("velocityX", 0, dampTime90, Time.deltaTime);
+//								print ("south");
+//						} else if (horizontal > 0) {
+//								animator.SetFloat ("velocityX", 1, dampTime90, Time.deltaTime);
+//								animator.SetFloat ("velocityY", 0, dampTime90, Time.deltaTime);
+//								print ("east");
+//						} else if (horizontal < 0) {
+//								animator.SetFloat ("velocityX", -1, dampTime90, Time.deltaTime);
+//								animator.SetFloat ("velocityY", 0, dampTime90, Time.deltaTime);
+//								print ("west");
+//						}
+//				}
+//				if (diagonal == true) {
+//
+//						if (horizontal < 0 && vertical < 0) {
+//								animator.SetFloat ("velocityX", -.5f, dampTime90, Time.deltaTime);
+//								animator.SetFloat ("velocityY", -.5f, dampTime90, Time.deltaTime);
+//								print ("southwest");
+//						} else if (horizontal > 0 && vertical < 0) {
+//								animator.SetFloat ("velocityX", .5f, dampTime90, Time.deltaTime);
+//								animator.SetFloat ("velocityY", -.5f, dampTime90, Time.deltaTime);
+//								print ("southeast");
+//						} else if (horizontal > 0 && vertical > 0) {
+//								animator.SetFloat ("velocityX", .5f, dampTime90, Time.deltaTime);
+//								animator.SetFloat ("velocityY", .5f, dampTime90, Time.deltaTime);
+//								print ("northeast");
+//						} else if (horizontal < 0 && vertical > 0) {
+//								animator.SetFloat ("velocityX", -.5f, dampTime90, Time.deltaTime);
+//								animator.SetFloat ("velocityY", .5f, dampTime90, Time.deltaTime);			
+//								print ("nortwest");
+//						}
+//				}
+//
+//
+//				if (horizontal == 0 && vertical == 0) {
+//						animator.SetFloat ("speed", 0);
+//						print ("idle");
+//				} else {
+//						animator.SetFloat ("speed", 1);
+//						print ("walking");
+//				}
 		}
-		else if (vertical < 0)
-		{
-			animator.SetInteger("Direction", 0);
-		}
-		else if (horizontal > 0)
-		{
-			animator.SetInteger("Direction", 1);
-		}
-		else if (horizontal < 0)
-		{
-			animator.SetInteger("Direction", 3);
-		}
-	}
 }
