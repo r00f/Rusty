22cbe75c12d6dd4bac474c6b2bc1c409bc1d5fbc
Dominik Würsch <d.wuersch@gmail.com>
2015-01-09 19:42:10 +0100

    Added SortingOrder Functionality for Walls to the GameController Script (all Walls have to be tagged "Wall" instead of having a SortingOrder script component).

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,29 +2,55 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
-	public Text cogAmountText;
-	private int cogAmount;
+		public Text cogAmountText;
+		private int cogAmount;
+		public Object walls;
+		public int sortingLayerID;
+		public int sortingOrder;
+		public int bounds;
 
-	// Use this for initialization
-	void Start () {
-		cogAmount = 0;
-		UpdateScore();
+		// Use this for initialization
+		void Start ()
+		{
+				cogAmount = 0;
+				UpdateScore ();
 
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
+		}
 
-	void UpdateScore() {
-		cogAmountText.text = "" + cogAmount;
-	}
+		void LateUpdate ()
+		{	
 
-	public void AddCog(int newCogValue) {
-		cogAmount += newCogValue;
-		UpdateScore ();
-	}
+				GameObject[] walls;
+				walls = GameObject.FindGameObjectsWithTag ("Wall");
+
+				foreach (GameObject wall in walls) {
+						SpriteRenderer wallSprite;
+						wallSprite = wall.GetComponent<SpriteRenderer> ();
+						wallSprite.sortingOrder = (int)Camera.main.WorldToScreenPoint (wallSprite.bounds.min).y * -1;
+			
+				}
+
+
+
+		}
+
+		void UpdateScore ()
+		{
+				cogAmountText.text = "" + cogAmount;
+		}
+
+		public void AddCog (int newCogValue)
+		{
+				cogAmount += newCogValue;
+				UpdateScore ();
+		}
 }
